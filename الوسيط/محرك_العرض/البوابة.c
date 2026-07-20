adb36cbeb7385d3d1e1366192296cd83b497ac33
مطور _العرب <saefhsayn@gmail.com>
2026-07-20 10:39:44 +0300

    Delete الوسيط directory

--- الوسيط/محرك_العرض/البوابة.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-#include "بوابة_النظام.h"
-#include "المحركات/خادم_العرض/الخادم.h"
-
-void _start(kernel_api_t* api)
-{
-    server_run(api);
-}
--- الوسيط/محرك_العرض/المحركات/خادم_العرض/الخادم.h
+++
@@ -1,9 +0,0 @@
-#ifndef الخادم_H
-#define الخادم_H
-
-#include <stdint.h>
-#include "بوابة_النظام.h"
-
-void server_run(kernel_api_t* api);
-
-#endif
